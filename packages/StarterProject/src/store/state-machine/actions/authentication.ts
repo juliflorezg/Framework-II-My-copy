5e87cf29da6381cc4c71fa3723a0bc3bed196de1
juliflorezg <forezjuli08@hotmail.com>
2023-01-27 10:11:35 -0500

    fix code style

--- packages/StarterProject/src/store/state-machine/actions/authentication.ts
+++ packages/StarterProject/src/store/state-machine/actions/authentication.ts
@@ -1,47 +1,47 @@
 export default {
-    setUTM: (context, event) => {
-        if (event?.user?.utm) {
-            context.user.utm = event.user.utm
-        }
-    },
-    setBusinessData: (context, event) => {
-        if (event?.user?.businessData) {
-            context.user.businessData = event.user.businessData
-        }
-    },
-    onLoading: (context, event) => {
-        if (event?.user) {
-            context.user = event.user
-        }
-    },
-    onSuccess: (context, event) => {
-        if (event.reverse) {
-            context.newLink = "/";
-        } else {
-            context.newLink = null;
-        }
-        context.errorMessage = null;
-    },
-    onError: (context, event) => {
-        if (event.reverse) {
-            context.newLink = null;
-        } else {
-            context.newLink = "/login";
-        }
-        context.errorMessage = event.errorMessage;
-    },
-    setUserData: (context, event) => {
-        if (event?.user) {
-            context.user = {
-                ...context.user,
-                ...event.user
-            }
-        }
-    },
-    logout: (context, event) => {
-        context.user = null
-    },
-    redirectDefault: (context, event) => {
-        event?.utils.Navigation.linkTo(event?.url)
+  setUTM: (context, event) => {
+    if (event?.user?.utm) {
+      context.user.utm = event.user.utm;
     }
-}
+  },
+  setBusinessData: (context, event) => {
+    if (event?.user?.businessData) {
+      context.user.businessData = event.user.businessData;
+    }
+  },
+  onLoading: (context, event) => {
+    if (event?.user) {
+      context.user = event.user;
+    }
+  },
+  onSuccess: (context, event) => {
+    if (event.reverse) {
+      context.newLink = '/';
+    } else {
+      context.newLink = null;
+    }
+    context.errorMessage = null;
+  },
+  onError: (context, event) => {
+    if (event.reverse) {
+      context.newLink = null;
+    } else {
+      context.newLink = '/login';
+    }
+    context.errorMessage = event.errorMessage;
+  },
+  setUserData: (context, event) => {
+    if (event?.user) {
+      context.user = {
+        ...context.user,
+        ...event.user,
+      };
+    }
+  },
+  logout: (context, event) => {
+    context.user = null;
+  },
+  redirectDefault: (context, event) => {
+    event?.utils.Navigation.linkTo(event?.url);
+  },
+};
